a059a45ad22c8a458d0f842a9067d233b981393a
15eddg <38993868+15eddg@users.noreply.github.com>
2018-07-20 21:43:57 -0400

    Public rooms stuff

--- app/src/main/java/com/example/haaris/newcopy5/SignInActivity.java
+++ app/src/main/java/com/example/haaris/newcopy5/SignInActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
