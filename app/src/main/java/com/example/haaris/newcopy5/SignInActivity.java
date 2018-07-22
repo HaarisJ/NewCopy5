e6fb3854d9e43b0072912742dd92336dc5430192
Haaris <haarisj22@hotmail.ca>
2018-07-22 14:27:21 -0400

    improved chat + restrictions on username

--- app/src/main/java/com/example/haaris/newcopy5/SignInActivity.java
+++ app/src/main/java/com/example/haaris/newcopy5/SignInActivity.java
@@ -3,7 +3,6 @@ package com.example.haaris.newcopy5;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
